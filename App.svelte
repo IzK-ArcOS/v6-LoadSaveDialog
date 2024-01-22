262e618aaa49625de3026074c2afbf8b87bc5a7b
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-22 16:37:10 +0100

    Format imports

--- App.svelte
+++ App.svelte
@@ -3,8 +3,8 @@
   import DirectoryViewer from "./Components/DirectoryViewer.svelte";
   import Header from "./Components/Header.svelte";
   import Toolbar from "./Components/Toolbar.svelte";
-  import { Runtime } from "./ts/runtime";
   import "./css/main.css";
+  import { Runtime } from "./ts/runtime";
 
   export let runtime: Runtime;
 </script>
